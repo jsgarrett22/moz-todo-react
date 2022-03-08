3feeb8d745d2b3568796df2ab618c5fd116b5a5b
jsgarrett22 <jgarrett35@student.cscc.edu>
2022-03-07 20:47:28 -0500

    -Added in-depth documentation for most core functionality
    -Added toggle sync between browser and code via the map iteration method and by adding callback prop to the Todo component
    
    -Added delete functionality of a task via the filter iteration method and by adding callback prop to the Todo component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import FilterButton from "./components/FilterButton";
 
 function App(props) {
 
+  // ====== sets up the state for the component to keep track of variables using {useState}
+  // ====== useState returns a variable 'tasks' and then a function that is used to update that variable 'setTasks'
+  // ====== Here we want to keep track of tasks that are passed via the property tasks typed in our component. This is
+  // ====== an array of objects with the properties - 'id', 'name', 'completed'
   const [tasks, setTasks] = useState(props.tasks);
 
   function addTask(name) {
@@ -14,23 +18,53 @@ function App(props) {
     setTasks([...tasks, newTask]); // copies tasklist and adds our new task at the end of the array and sets it to 'tasks'
   }
 
+  // ====== this function is ran when a checkbox is toggled on or off. In order to sync that state between the browser and our code,
+  // ====== we need to iterate through all the tasks, and find the checkbox that has had the onChange event fired. That will be the id argument passed
+  // ====== into this function. If we compare that with each iterations id in the tasklist named 'tasks', we need to use object spread syntax to make a new
+  // ====== object whose 'completed' prop has been inverted and return it (add it to the updatedTasks array created by the map function). If the current
+  // ====== iteration of tasks id doesn't match, then just add that to the updatedTasks array. At the end of the function, lets make sure we update our
+  // ====== tasks with our new updatedTasks array
   function toggleTaskCompleted(id) {
-    console.log(tasks[0]);
+    const updatedTasks = tasks.map(task => {
+      // if this task has the same ID as the edited task
+      if (id === task.id) {
+        // use object spread to make a new object
+        // whose 'completed' prop has been inverted
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
   }
 
-  // grab all tasks and return a 'Todo' component with each task's attributes
+  // ====== this function is ran when the delete button of the task is clicked. We filter through all the tasks that don't match the current id because,
+  // ====== the current id is the task that is wanting to be deleted. Everything else is filtered into a new array and that new array is passed into setTasks
+  // ====== to update our tasks
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  // ====== for every task object in our tasks property, we are going to map or 'do something' for each one.
+  // ====== A Todo component will be created and each property will be populated using the current task's properties
+  // ====== We need to make a special key property as a unique identifier for indexing purposes. An id is usually suitable for this
   const taskList = tasks.map(task => (
     <Todo 
       id={task.id} 
       name={task.name} 
       completed={task.completed}
       key={task.id}               // this is used as a special prop to index / order by React and cannot be used for any other purpose
+      toggleTaskCompleted={toggleTaskCompleted} // we include this so we can access it inside the Todo component and attach it to an event handler
+      deleteTask={deleteTask} // we include this so we can access it inside the Todo component and attach it to an event handler
     />
   ));
 
+
+  // ====== changes num of tasks heading to reflect number of tasks in tasklist ======
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${tasks.length} ${tasksNoun} remaining`;
 
+  // ====== returns an app component, which houses our entire app for index.js
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import App from './App';
 
 const DATA = [
   { id: "todo-0", name: "Eat", completed: true},
-  //{ id: "todo-1", name: "Sleep", completed: false},
-  //{ id: "todo-2", name: "Repeat", completed: false},
+  { id: "todo-1", name: "Sleep", completed: false},
+  { id: "todo-2", name: "Repeat", completed: false},
 ];
 
+// ====== this is the entry point for the react app. We initialize our app component within the ReactDOM.render() method's first parameter, followed
+// ====== by a root element that is depicted in index.html as 'root'
 ReactDOM.render(
   <React.StrictMode>
     <App tasks={DATA}/>
